0f7e10182006311fc475c51a2a6906485d0c729c
Don Park <don@donpark.org>
2012-06-20 14:47:43 -0700

    config and socket setup

--- treadmilldb.rs
+++ treadmilldb.rs
@@ -1,9 +1,41 @@
 // treadmilldb
+use epoll;
+use socket;
+
+type config = {port: u16};
 
 fn main(args: [str]) {
-  // event listener
-  //
+  // listen to http
+  let config = config();
+  let epoll_fd = setup(config);
 
   io::println("treadmill db has started. time to get moving.")
+  listen(epoll_fd);
 }
 
+fn config() -> config {
+  // load from file
+  ret {port: 1444_u16};
+}
+
+fn setup(config: config) -> int {
+  let ep = epoll::epoll_create();
+
+  alt socket::bind_socket("localhost", config.port) {
+    result::ok(s) {
+      alt socket::listen(s, 1) {
+        result::ok(socket) {
+          io::println("Listening on "+#fmt("%u", config.port as uint));
+        }
+        result::err(e) {
+          io::println(#fmt("listen error: %s", e));
+        }
+      }
+    }
+    result::err(e) {
+      io::println(#fmt("bind error: %s", e));
+    }
+  }
+
+  ret ep;
+}
